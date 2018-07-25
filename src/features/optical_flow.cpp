9173b25c03e45de7da215e7d922fec9b85da8188
Harashiyo <shohei.h1024@gmail.com>
2018-07-25 19:41:14 +0900

    feature template

---
+++ src/features/optical_flow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Haraoka Shohei on 2018/07/25.
+//
+
+#include "optical_flow.h"
---
+++ src/features/optical_flow.h
@@ -0,0 +1,20 @@
+//
+// Created by Haraoka Shohei on 2018/07/25.
+//
+
+#ifndef PITCHANGLECORRECTION_OPTICAL_FLOW_H
+#define PITCHANGLECORRECTION_OPTICAL_FLOW_H
+
+namespace openMVG {
+    namespace features {
+        class Eemployee
+        {
+        public:
+            std::string name;       //!< 氏名
+            std::string address;    //!< 住所
+        };
+
+    }
+}
+
+#endif //PITCHANGLECORRECTION_OPTICAL_FLOW_H
